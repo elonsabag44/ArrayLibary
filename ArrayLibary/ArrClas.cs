17f25536d1daff64428532c8a3d4567e8980a928
Owner <Owner@CND34211C2>
2026-09-03 12:00:04 +0300

    Add project files.

---
+++ ArrayLibary/ArrClas.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayLibary
+{
+    public class ArrClas
+    {
+        public class MyArrayLibary
+        {
+            /// <summary>
+            /// פעולה מקבלת מערך מספרים שלמים 
+            /// פעולה ממלאה מערך בנתונים 
+            /// </summary>
+            /// <param name="arr"></param>
+            public void InputArray(int[] arr)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine("Enter the value for index " + i + ":");
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            public void RandomArray(int[] arr, int min, int max)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine("Enter the value for index " + i + ":");
+                    arr[i] = new Random().Next(min, max + 1);
+                }
+            }
+        }
+    }
+}
---
+++ mesima1array/Program.cs
@@ -0,0 +1,19 @@
+﻿using ArrayLibary;
+namespace mesima1array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            targil1();
+        }
+        static void targil1()
+        {
+            int[] arr = new int[50];
+            MyArrayLibary myArrayLibary = new MyArrayLibary();
+            myArrayLibary.inputArray(arr);
+            
+        }
+
+    }
+}
